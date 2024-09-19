1cecea4e3d61b389616bb8e222e42c3a1d37c192
thunderkite <hanasisgenya200710@gmail.com>
2024-09-19 20:52:44 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var i int
+	fmt.Print("Введите число i: ")
+	fmt.Scan(&i)
+	switch {
+	case i > 0:
+		fmt.Println("i больше нуля")
+	case i < 0:
+		fmt.Println("i меньше нуля")
+	default:
+		fmt.Println("i равно нулю")
+	}
 }
